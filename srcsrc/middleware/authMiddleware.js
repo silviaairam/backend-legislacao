5ec1a5a21cd2b69aec090bb3e3630eff0411c80f
silviaairam <silviaairam@gmail.com>
2025-05-22 20:51:36 -0300

    Update authMiddleware.js

--- srcsrc/middleware/authMiddleware.js
+++ srcsrc/middleware/authMiddleware.js
@@ -1,7 +1,1 @@
-function authMiddleware(req, res, next) {
-  // Aqui você pode validar o token ou autenticação
-  console.log("Middleware de autenticação ativado");
-  next();
-}
 
-module.exports = authMiddleware;
