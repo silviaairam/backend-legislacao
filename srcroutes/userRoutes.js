17d8f7215cf5b4efc102161a460c1cefbdd0ad2d
silviaairam <silviaairam@gmail.com>
2025-05-22 20:51:03 -0300

    Update userRoutes.js

--- srcroutes/userRoutes.js
+++ srcroutes/userRoutes.js
@@ -1,12 +1,1 @@
-const express = require("express");
-const router = express.Router();
-const { authMiddleware } = require("../middleware/authMiddleware");
-const {
-  getUserProfile,
-  updateUserProfile
-} = require("../controllers/userController"); // crie este controller se ainda não tiver
 
-router.get("/profile", authMiddleware, getUserProfile);
-router.put("/profile", authMiddleware, updateUserProfile);
-
-module.exports = router;
